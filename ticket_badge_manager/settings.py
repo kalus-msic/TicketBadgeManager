73e9b53d8ef631c9077b9dc7d732621fd61ed217
Michael Kalus <michael.kalus@ms-ic.cz>
2025-02-25 21:45:42 +0100

    secret_key, allowed_hosts

--- ticket_badge_manager/settings.py
+++ ticket_badge_manager/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$ligfi$fz(xj6)bnnjhmq#oab@^2pxsh=g+gho8u4blb@e6i-='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
