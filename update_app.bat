7ca61467774d4a48e6e188973b0edbcfa526cb1c
Michael Kalus <michael.kalus@ms-ic.cz>
2025-03-19 23:53:42 +0100

    Remove diacritics

--- update_app.bat
+++ update_app.bat
@@ -1,6 +1,6 @@
 @echo off
 REM --- Zkontrolujeme aktuálnost git repozitáře ---
-echo Kontroluji aktualizace v repozitáři...
+echo Kontroluji aktualizace v repozitari...
 git fetch
 
 REM Zjistíme hash aktuálního commitu a vzdálené větve
@@ -8,21 +8,21 @@ for /f "tokens=*" %%i in ('git rev-parse HEAD') do set LOCAL=%%i
 for /f "tokens=*" %%i in ('git rev-parse @{u}') do set REMOTE=%%i
 
 if "%LOCAL%"=="%REMOTE%" (
-    echo Repozitář je aktualizován.
+    echo Repozitar je aktualni.
 ) else (
-    echo Nalezena nová verze repozitáře.
+    echo Nalezena nová verze repozitare.
     choice /m "Chcete stáhnout aktualizace z Gitu? (Y/N)"
     if errorlevel 2 (
         echo Aktualizace nebyla provedena.
         goto :end
     ) else (
-        echo Provádím aktualizaci...
+        echo Provadim aktualizaci...
         git pull
     )
 )
 
 REM --- Aktivace virtuálního prostředí a migrace ---
-echo Aktivace virtuálního prostředí...
+echo Aktivace virtualniho prostredi...
 call venvTBM\Scripts\activate
 
 echo Spoustim makemigrations...
