237434c4b43d5848cc1ca6951c6422da25064262
Michael Kalus <michael.kalus@ms-ic.cz>
2025-06-11 02:37:58 +0200

    qr for app url in settings

--- tickets/views/qr_views.py
+++ tickets/views/qr_views.py
@@ -5,12 +5,25 @@ from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
 from ..models import Ticket
 from ..decorators import login_required_ajax
+from ..utils.validators import sanitize_string
 
 
 @login_required_ajax
-def generate_qr_code(request, ticket_id):
-    """Generate QR code for a ticket."""
-    ticket = get_object_or_404(Ticket, pk=ticket_id)
+def generate_qr_code(request, ticket_id=None):
+    """Generate QR code for a ticket or any URL."""
+    # Check if we should generate QR for URL (from GET parameter)
+    url_param = request.GET.get('url', '')
+    
+    if url_param:
+        # Generate QR for URL
+        data = url_param
+    elif ticket_id:
+        # Generate QR for ticket
+        ticket = get_object_or_404(Ticket, pk=ticket_id)
+        data = ticket.qr_code
+    else:
+        # No data provided
+        return HttpResponse(status=400)
     
     # Create QR code
     qr = qrcode.QRCode(
@@ -19,7 +32,7 @@ def generate_qr_code(request, ticket_id):
         box_size=10,
         border=4,
     )
-    qr.add_data(ticket.qr_code)
+    qr.add_data(data)
     qr.make(fit=True)
     
     # Create an image
